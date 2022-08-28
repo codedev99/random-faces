9751f03cdc0bca6d30aa106cdd4d289880635b30
Ved Gupta <vedguptaup@gmail.com>
2022-08-28 13:02:32 +0530

    setup, changed folder name to streamlit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/.random-faces/
+mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
@@ -6,4 +6,4 @@ headless=true\n\
 port=$PORT\n\
 enableCORS=false\n\
 \n\
-" > ~/.random-faces/config.toml
+" > ~/.streamlit/config.toml

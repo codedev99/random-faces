d6467bda870f6d61cbd49eeb961aa19e7cddef84
Ved Gupta <vedguptaup@gmail.com>
2022-08-28 14:49:38 +0530

    torch test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy
 import torch
-# import torch.jit as jit
 
 st.write("Success")
 a = torch.Tensor([1])
+st.write(type(a))
+gen = torch.jit.load("./scripted_generator.pt")
